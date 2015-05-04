e4e2d11a99464bdc61658c45a75231eb3be00d11
Nikola Henezi <nikola@henezi.com>
2015-05-04 10:04:22 +0200

    Adding algorithm for determining winner

--- app/Game.php
+++ app/Game.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model {
+  const BOARD_HEIGHT = 7;
+  const BOARD_WIDTH = 7;
+
   protected $table = 'games';
   public $timestamps = false;
 
@@ -14,7 +17,7 @@ class Game extends Model {
     $last_move = 0;
     foreach ($this->moves as $move) {
       if ($move->number > $last_move) {
-        $last_move = $move->number;
+        $last_move = $move;
       }
     }
 
@@ -32,5 +35,105 @@ class Game extends Model {
     }
 
     return $moves;
+  }
+
+  protected function initBoard() {
+    $grid = [];
+    for ($i = 0; $i < self::BOARD_WIDTH; $i++) {
+      $row = [];
+      for ($j = 0; $j < self::BOARD_WIDTH; $j++) {
+        $row[] = 0;
+      }
+      $grid[] = $row;
+    }
+
+    return $grid;
+  }
+
+  public function getBoard() {
+    $board = $this->initBoard();
+    foreach ($this->moves as $move) {
+      $board[$move->y][$move->x] = $move->player_id;
+    }
+    return $board;
+  }
+
+  public function wasLastMoveWinning($board = null, $last_move = null) {
+    if ($board == null) {
+      $board = $this->getBoard();
+    }
+    if ($last_move == null) {
+      $last_move = $this->getLastMove();
+    }
+    $x = $last_move->x;
+    $y = $last_move->y;
+
+    $in_a_row = 0;
+    // check if there is a horizontal 4 in a row
+    $sp = ($x - 3 < 0) ? 0 : $x - 3; // starting point
+    for ($i = $sp; $i < self::BOARD_WIDTH && $i <= $x + 3; $i++) {
+      if ($board[$i][$y] === $last_move->player_id) {
+        $in_a_row++;
+      } else {
+        $in_a_row = 0;
+      }
+
+      if ($in_a_row >= 4) {
+        return true;
+      }
+    }
+
+    $in_a_row = 0;
+    // check if there is a vertical 4 in a row
+    $sp = ($y - 3 < 0) ? 0 : $y - 3;
+    for ($i = $sp; $i < self::BOARD_HEIGHT && $i <= $y + 3; $i++) {
+      if ($board[$x][$i] === $last_move->player_id) {
+        $in_a_row++;
+      } else {
+        $in_a_row = 0;
+      }
+
+      if ($in_a_row >= 4) {
+        return true;
+      }
+    }
+
+    $in_a_row = 0;
+    // check if there is a diagonal (top left -> bottom right) 4 in a row
+    for ($i = -3, $j = 3; $x + $i <= $x + 3, $y + $j >= $y - 3; $i++, $j--) {
+      if ($x + $i < 0 || $x + $i >= self::BOARD_WIDTH || $y + $j < 0 || $y + $j >= self::BOARD_WIDTH) {
+        continue;
+      }
+
+      if ($board[$x + $i][$y + $j] == $last_move->player_id) {
+        $in_a_row++;
+      } else {
+        $in_a_row = 0;
+      }
+
+      if ($in_a_row >= 4) {
+        return true;
+      }
+    }
+
+    $in_a_row = 0;
+    // check if there is a diagonal (bottom left -> top right) 4 in a row
+    for ($i = -3, $j = -3; $x + $i <= $x + 3, $y + $j <= $y + 3; $i++, $j++) {
+      if ($x + $i < 0 || $x + $i >= self::BOARD_WIDTH || $y + $j < 0 || $y + $j >= self::BOARD_WIDTH) {
+        continue;
+      }
+
+      if ($board[$x + $i][$y + $j] == $last_move->player_id) {
+        $in_a_row++;
+      } else {
+        $in_a_row = 0;
+      }
+
+      if ($in_a_row >= 4) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
---
+++ tests/GameTest.php
@@ -0,0 +1,377 @@
+<?php
+
+use \App\Game;
+
+class GameTest extends TestCase {
+
+  public function testEmptyGrid() {
+    $board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 2;
+    $last_move->y = 0;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+  }
+
+  public function testCorners() {
+    $board = [
+      [1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 0;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 6;
+    $last_move->y = 0;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 6;
+    $last_move->y = 6;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [0, 0, 0, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 6;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+  }
+
+  public function testHorizontalWins() {
+    $board = [
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 4;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 0, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 1;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 6;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+
+    $board = [
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 1;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 2, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [2, 1, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 1;
+    $last_move->y = 1;
+    $last_move->player_id = 1;
+    $game = new Game();
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+  }
+
+
+  public function testVerticalWins() {
+    $game = new Game();
+
+    $board = [
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 3;
+    $last_move->y = 3;
+    $last_move->player_id = 1;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+
+    $board = [
+      [2, 0, 0, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 3;
+    $last_move->y = 0;
+    $last_move->player_id = 1;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [1, 2, 2, 1, 1, 1, 1],
+      [1, 2, 1, 2, 1, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [2, 1, 1, 2, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 6;
+    $last_move->player_id = 1;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+
+    $board = [
+      [1, 2, 2, 1, 1, 2, 1],
+      [1, 2, 1, 2, 1, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [2, 1, 1, 2, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 6;
+    $last_move->player_id = 1;
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 3;
+    $last_move->player_id = 1;
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+  }
+
+  public function testTLBRWins() {
+    $game = new Game();
+
+    $board = [
+      [2, 2, 2, 1, 0, 0, 0],
+      [1, 2, 1, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 2, 1, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 3;
+    $last_move->player_id = 1;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 1, 2, 2, 1, 1, 2],
+      [1, 2, 1, 1, 2, 2, 0],
+      [2, 1, 2, 1, 2, 2, 0],
+      [1, 1, 2, 2, 1, 0, 0],
+      [2, 2, 1, 1, 2, 0, 0],
+      [1, 2, 1, 2, 0, 0, 0],
+      [2, 2, 1, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 6;
+    $last_move->player_id = 2;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 1, 2, 2, 1, 1, 2],
+      [1, 2, 1, 1, 2, 2, 0],
+      [2, 1, 2, 1, 1, 2, 0],
+      [1, 1, 2, 2, 1, 0, 0],
+      [2, 2, 2, 1, 2, 0, 0],
+      [1, 2, 1, 2, 0, 0, 0],
+      [2, 2, 1, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 3;
+    $last_move->y = 3;
+    $last_move->player_id = 2;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 1, 2, 2, 1, 1, 2],
+      [1, 2, 1, 1, 2, 2, 0],
+      [2, 1, 2, 1, 1, 2, 0],
+      [1, 1, 2, 2, 2, 0, 0],
+      [2, 2, 2, 1, 2, 0, 0],
+      [1, 2, 1, 2, 0, 0, 0],
+      [2, 2, 1, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 2;
+    $last_move->y = 3;
+    $last_move->player_id = 1;
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+  }
+
+
+  public function testBLTRWins() {
+    $game = new Game();
+
+    $board = [
+      [2, 2, 1, 2, 0, 0, 0],
+      [1, 2, 1, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 2, 2, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 0;
+    $last_move->player_id = 2;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 2, 1, 2, 0, 0, 0],
+      [1, 1, 1, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 2, 2, 0, 0, 0],
+      [2, 2, 2, 0, 0, 0, 0],
+      [1, 2, 0, 0, 0, 0, 0],
+      [2, 2, 0, 0, 0, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 0;
+    $last_move->y = 0;
+    $last_move->player_id = 2;
+    $this->assertEquals(false, $game->wasLastMoveWinning($board, $last_move));
+
+    $board = [
+      [2, 2, 1, 2, 0, 0, 0],
+      [1, 1, 1, 0, 0, 0, 0],
+      [2, 1, 2, 0, 0, 0, 0],
+      [1, 1, 2, 2, 0, 0, 0],
+      [2, 2, 1, 0, 0, 0, 0],
+      [1, 2, 2, 1, 0, 0, 0],
+      [2, 2, 1, 2, 1, 0, 0],
+    ];
+    $last_move = new stdClass();
+    $last_move->x = 6;
+    $last_move->y = 4;
+    $last_move->player_id = 1;
+    $this->assertEquals(true, $game->wasLastMoveWinning($board, $last_move));
+  }
+}
